1b9463934c2f3442cd31110a1a6ca2ecd79aa27f
hkalife <henriquekalife@gmail.com>
2021-03-17 21:37:31 -0300

    feat(firebase): implement authentication

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+public class AuthManager : MonoBehaviour {
+  //Firebase variables
+  [Header("Firebase")]
+  public DependencyStatus dependencyStatus;
+  public FirebaseAuth auth;    
+  public FirebaseUser User;
+
+  //Login variables
+  [Header("Login")]
+  public TMP_InputField emailLoginField;
+  public TMP_InputField passwordLoginField;
+  public TMP_Text warningLoginText;
+  // public TMP_Text confirmLoginText;
+
+  void Awake() {
+    //checando dependências
+    FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+    {
+      dependencyStatus = task.Result;
+      if (dependencyStatus == DependencyStatus.Available) {
+        //iniciar firebase
+        InitializeFirebase();
+      }
+      else {
+        Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+      }
+    });
+  }
+
+  private void InitializeFirebase() {
+    Debug.Log("Setting up Firebase Auth");
+    //Set the authentication instance object
+    auth = FirebaseAuth.DefaultInstance;
+  }
+
+  //Function for the login button
+  public void LoginButton() {
+    //Call the login coroutine passing the email and password
+    StartCoroutine(Login(emailLoginField.text, passwordLoginField.text));
+  }
+
+  private IEnumerator Login(string _email, string _password) {
+    //Call the Firebase auth signin function passing the email and password
+    var LoginTask = auth.SignInWithEmailAndPasswordAsync(_email, _password);
+    //Wait until the task completes
+    yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+    if (LoginTask.Exception != null) {
+      //If there are errors handle them
+      Debug.LogWarning(message: $"Failed to register task with {LoginTask.Exception}");
+      FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+      AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+      string message = "Erro de login, tente novamente.";
+      switch (errorCode) {
+        case AuthError.MissingEmail:
+          message = "Verifique o E-mail";
+          break;
+        case AuthError.MissingPassword:
+          message = "Verifique a senha";
+          break;
+        case AuthError.WrongPassword:
+          message = "Senha errada";
+          break;
+        case AuthError.InvalidEmail:
+          message = "Email fora de formato";
+          break;
+        case AuthError.UserNotFound:
+          message = "Conta inexistente";
+          break;
+      }
+      warningLoginText.text = message;
+    }
+    else {
+      //User is now logged in
+      //Now get the result
+      User = LoginTask.Result;
+      Debug.LogFormat("User signed in successfully: {0} ({1})", User.DisplayName, User.Email);
+
+      MainMenu menuController = gameObject.GetComponent<MainMenu>();
+      menuController.GoToThirdMenu();
+
+      warningLoginText.text = "";
+    }
+  }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
 
-    public void QuitGame() {
-        Application.Quit();
+  [SerializeField]
+  private GameObject firstMenu;
+
+  [SerializeField]
+  private GameObject secondMenu;
+
+  [SerializeField]
+  private GameObject thirdMenu;
+
+  public TMP_Text warningLoginText;
+
+
+  public void PlayGame() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  void Update() {
+    if (firstMenu.activeInHierarchy) {
+      warningLoginText.text = "";
     }
+  }
+
+  public void QuitGame() {
+    Application.Quit();
+  }
+
+  public void GoToFirstMenu() {
+    firstMenu.SetActive(true);
+    secondMenu.SetActive(false);
+    thirdMenu.SetActive(false);
+  }
+
+  public void GoToSecondMenu() {
+    firstMenu.SetActive(false);
+    secondMenu.SetActive(true);
+    thirdMenu.SetActive(false);
+  }
+
+  public void GoToThirdMenu() {
+    firstMenu.SetActive(false);
+    secondMenu.SetActive(false);
+    thirdMenu.SetActive(true);
+  }
+
+  public void OpenAeonSpaceSystem() {
+    Application.OpenURL("http://unity3d.com/");
+  }
 }
